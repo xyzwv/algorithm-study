313624121b34b919aec2b672787cfad4308500f5
YEJI <121334671+YJ-Lee-Kim@users.noreply.github.com>
2023-03-21 15:55:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int result = str.charAt(0);		
+		
+		System.out.print(result);
+		
+	}
+
+}
