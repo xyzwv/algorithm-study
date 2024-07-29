e83014e965c5ad5f8d174b79f5db93468a5b3e88
yeji <yeji.lee.kim@gmail.com>
2024-07-29 23:05:36 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,37 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-stack<int> s;
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     
+    int n;
     string cmd;
+    stack<int> st;
     
-    for(int i=0; i<t; i++){
-        getline(cin, cmd);
-        
-        if(cmd.size() == 3){
-            if(s.empty()) cout << -1 <<"\n";
-            else if(cmd.compare("top") == 0) cout << s.top() << "\n";
-            else {
-                cout << s.top() << "\n";
-                s.pop();
+    cin >> n;
+    
+    while(n--){
+        cin >> cmd;
+        if(cmd == "push"){
+            int x;
+            cin >> x;
+            st.push(x);
+        } else if(cmd == "pop"){
+            if(!st.empty()) {
+                cout << st.top() << "\n";
+                st.pop();
+            } else {
+                cout << "-1\n";
+            }
+        } else if(cmd == "size"){
+            cout << st.size() << "\n";
+        } else if(cmd == "empty"){
+            cout << st.empty() << "\n";
+        } else if(cmd == "top"){
+            if(!st.empty()) {
+                cout << st.top() << "\n";
+            } else {
+                cout << "-1\n";
             }
         }
-        else if(cmd.size() == 4){
-            cout << s.size() << "\n";
-        }
-        else if(cmd.size() == 5){
-            cout << (int)s.empty() << "\n";
-        }
-        else {
-            s.push(stoi(cmd.substr(5)));
-        }
-        
     }
-	
     return 0;
 }
