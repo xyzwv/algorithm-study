6ee472daff977b576cb60e3bec6956b6aea3fac1
yeji <yeji.lee.kim@gmail.com>
2024-08-07 22:52:05 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6124 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[1025][1025];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cin >> arr[i][j];
+            arr[i][j] += arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
+        }
+    }
+    
+    int x1, x2, y1, y2;
+    while(m--){
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1] << "\n";
+    }
+
+    return 0;
+}
