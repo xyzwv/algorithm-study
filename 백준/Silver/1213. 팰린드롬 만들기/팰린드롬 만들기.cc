9695f1f64e143c575e66ab5fc049a30aaae4b02f
yeji <yeji.lee.kim@gmail.com>
2024-09-24 15:33:21 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,72 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-string makeNewHalf(int* alphabet) {
-    string result = "";
-    
-    for(int i = 0; i < 26; i++) {
-        if(alphabet[i] == 0) continue;
-        
-        for(int j = 0; j < alphabet[i] / 2; j++){
-            result += (char)('A' + i);
-        }
+const int SIZE = 26;
+
+vector<int> countAlphabet(string str) {
+    vector<int> v(SIZE, 0);
+    for(int i=0; i<str.size(); i++){
+        v[str[i] - 'A']++;
     }
-    
-    return result;
+    return v;
 }
 
-string makeRestHalf(string str, bool is_odd) {
-    int start = (is_odd) ? str.size() - 2 : str.size() - 1;
-    for(int i = start; i >= 0; i--) {
-        str += str[i];
-    }
-    return str;
-}
+string makePalindrome(vector<int>& count) {
+    string part1 = "", part2 = "", part3 = "";
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    int i;
-    string input, answer="";
-    int alphabet[26];
-    for(i=0; i<26; i++) {
-        alphabet[i] = 0;
-    }
-    
-    // 입력
-    cin >> input;
-    
-    // 연산
-    i=0;
-    while(i < input.size()) {
-        alphabet[input[i++] - 'A']++;
-    }
-    
-    int odd_count = 0;
-    int odd_idx = 0;
-    for(i=0; i<26; i++) {
-        if(alphabet[i] % 2 == 1) {
-            odd_count++;
-            odd_idx = i;
+    for(int i=0; i<SIZE; i++){
+        if(count[i] % 2 == 1){
+            if(part2.size() == 1) return "I'm Sorry Hansoo";
+
+            part2 = (char)(i + 'A');
+        }
+
+        for(int j=0; j<count[i]/2; j++){
+            part1 += (char)(i + 'A');
+            part3 = (char)(i + 'A') + part3;
         }
     }
-    
-    if(odd_count >= 2) { // 홀수개인 알파벳이 2개 이상
-        cout << "I'm Sorry Hansoo";
-        return 0;
-    } else if(odd_count == 1) { // 홀수개인 알파벳이 1개
-        alphabet[odd_idx]--;
-        answer += makeRestHalf(makeNewHalf(alphabet) + (char)('A' + odd_idx), true);
-    } else { // 모든 알파벳이 짝수개
-        answer = makeRestHalf(makeNewHalf(alphabet), false);
-    }
-    
-    // 출력
-    cout << answer;
-    
-    return 0;
+
+    return part1 + part2 + part3;
+}
+
+string solution(string str) {
+    vector<int> count = countAlphabet(str);
+    return makePalindrome(count);
+}
+
+int main() {
+	string input;
+	cin >> input;
+
+	cout << solution(input);
+
+	return 0;
 }
