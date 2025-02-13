7ea39ce7b5c5c5db4873767f39bb94f3ca1a0de3
yeji <yeji.lee.kim@gmail.com>
2025-02-14 00:38:58 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, x;
+    cin >> n;
+    
+    priority_queue<int, vector<int>, greater<int>> heap;
+    while(n--){
+        cin >> x;
+        
+        if(x == 0) {
+            if(heap.empty()) cout << "0\n";
+            else {
+                cout << heap.top() << "\n";
+                heap.pop();
+            }
+        }
+        else heap.push(x);
+    }
+    
+    return 0;
+}
