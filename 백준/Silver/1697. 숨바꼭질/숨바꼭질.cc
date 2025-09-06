444f3f2fe547bb730cc979e2fa9092aff391768b
yeji <yeji.lee.kim@gmail.com>
2025-09-06 22:32:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,58 +1,49 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int MAX = 100000;
 
-vector<int> ways(int pos) {
-    vector<int> v;
-    int dx[] = { -1, 1, pos };
-    
+vector<int> moves(int pos) {
+    int dx[] = {-1, 1, pos};
+    vector<int> res;
     for(int i=0; i<3; i++) {
-        int x = pos + dx[i];
-        if(x >= 0 && x <= MAX ) {
-            v.push_back(x);
-        }
-    }
-    
-    return v;
+        int x = dx[i] + pos;
+        if(x >= 0 && x <= MAX) res.push_back(x);
+    } 
+    return res;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int n, k, ans;
-    bool visited[MAX+1] = {};
-    cin >> n >> k;
-    
+
+    int N, K, ans = 0;
+    cin >> N >> K;
+
     queue<pair<int, int>> q;
-    q.push({n, 0});
-    visited[n] = true;
-    
+    q.push({N, 0});
+
+    vector<bool> visited(MAX + 1, false);
+
     while(!q.empty()) {
         int pos = q.front().first;
         int sec = q.front().second;
         q.pop();
-        
-        if(pos == k) {
+        visited[N] = true;
+
+        if(pos == K) {
             ans = sec;
             break;
         }
-        
-        visited[pos] = true;
-        
-        for(auto w : ways(pos)) {
-            if(!visited[w]) {
-                q.push({w, sec+1});
-                visited[w] = true;
-            }
+
+        for(int next : moves(pos)) {
+            if(visited[next]) continue;
+            q.push({next, sec + 1});
+            visited[next] = true;
         }
     }
-    
+
     cout << ans;
-    
     return 0;
 }
