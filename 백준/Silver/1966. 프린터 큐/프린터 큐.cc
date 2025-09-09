01d494996ee55efff3fdf19ae3a4c730adfc6cb1
yeji <yeji.lee.kim@gmail.com>
2025-09-09 13:07:12 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef pair<int, int> pi;
-
-int getMax(int* cnt) {
+int getMax(int* priority) {
     for(int i=9; i>0; i--) {
-        if(cnt[i]) return i;
+        if(priority[i] > 0) return i;
     }
     return 0;
 }
@@ -14,38 +11,39 @@ int getMax(int* cnt) {
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int t;
-    cin >> t;
-    while(t--) {
-        int n, m;
-        cin >> n >> m;
+    cin.tie(NULL); cout.tie(NULL);
 
-        queue<pi> q;
-        int cnt[10] = {0};
-        for(int i=0; i<n; i++) {
-            int x;
-            cin >> x;
-            q.push({x, i});
-            cnt[x]++;
+    int T;
+    cin >> T;
+    
+    while(T--) {
+        int N, M;
+        cin >> N >> M;
+
+        int ans = 0;
+        int priority[10] = {0};
+        queue<pair<int, int>> q;
+        for(int i=0; i<N; i++) {
+            int num;
+            cin >> num;
+            q.push({num, i});
+            priority[num]++;
         }
 
-        int turn = 0;
         while(!q.empty()) {
-            pi curr = q.front();
-            q.pop();
-            if(curr.first == getMax(cnt)) {
-                cnt[curr.first]--;
-                turn++;
-                if(curr.second == m) {
-                    cout << turn << "\n";
-                    break;
+            auto [p, o] = q.front();
+            q.pop();  
+            
+            if(p == getMax(priority)) {
+                priority[p]--;
+                ans++;
+                if(o == M) {
+                    cout << ans << "\n";
                 }
             } else {
-                q.push(curr);
+                q.push({p, o});
             }
+
         }
     }
     
