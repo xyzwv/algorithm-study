1671dae89e82206918ac3ca84cab8857fc364a50
yeji <yeji.lee.kim@gmail.com>
2025-10-28 16:44:52 +0900

    [level 1] Title: 폰켓몬, Time: 5.71 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums) set.add(n);
+        if(set.size() >= nums.length / 2) return nums.length / 2;
+        else return set.size();
+    }
+}
