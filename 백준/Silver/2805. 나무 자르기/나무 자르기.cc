9c10c97ff630c0cdcb590fd945edeaac05ebd4b1
yeji <yeji.lee.kim@gmail.com>
2025-10-30 23:12:08 +0900

    [Silver II] Title: 나무 자르기, Time: 140 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -5,36 +5,32 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
-    int N;
-    long long M;
+
+    int N, M;
     cin >> N >> M;
 
-    vector<int> v(N);
-    int low = 0, high = 0;
-    for(int i=0; i<N; i++) {
-        cin >> v[i];
-        high = max(v[i], high);
-    }
+    vector<int> tree(N);
+    for(int i=0; i<N; i++) cin >> tree[i];
 
-    int ans = 0;
-    while(low <= high) {
-        int height = (low + high) / 2;
-        
-        long long sum = 0;
+    int answer = 0;
+    int max_height = *max_element(tree.begin(), tree.end());
+    int min_height = 0;
+    while(min_height <= max_height) {
+        int middle_height = (max_height + min_height) / 2;
+        long long cut = 0;
         for(int i=0; i<N; i++) {
-            if(v[i] <= height) continue;
-            sum += v[i] - height;
+            if(tree[i] <= middle_height) continue;
+            cut += tree[i] - middle_height;
         }
-
-        if(sum >= M) {
-            ans = height;
-            low = height + 1;
+        if(cut >= M) {
+            answer = middle_height;
+            min_height = middle_height + 1;
         } else {
-            high = height - 1;
+            max_height = middle_height - 1;
         }
     }
     
-    cout << ans;
+    cout << answer;
+
     return 0;
 }
