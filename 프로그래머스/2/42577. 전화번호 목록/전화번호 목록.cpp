1d14926d239f6ade96ad63ab3828681ceb7a997e
yeji <yeji.lee.kim@gmail.com>
2025-10-31 11:35:26 +0900

    [level 2] Title: 전화번호 목록, Time: 81.80 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -2,20 +2,12 @@
 
 using namespace std;
 
-bool isPrefix(string a, string b) {
-    return equal(a.begin(), a.end(), b.begin());
-}
-
 bool solution(vector<string> phone_book) {
     sort(phone_book.begin(), phone_book.end());
-    
     for(int i=0; i<phone_book.size()-1; i++) {
-        string s1 = phone_book[i];
-        string s2 = phone_book[i+1];
-        if(s1.size() < s2.size() && s1[0] == s2[0] && isPrefix(s1, s2)) {
-           return false;
-        }
+        string cur = phone_book[i];
+        string next = phone_book[i+1];
+        if(cur.size() < next.size() && next.substr(0, cur.size()) == cur) return false;
     }
-    
     return true;
 }
