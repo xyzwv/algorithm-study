d6faa89fc4cf8a70fd40e32472f2aa2c01a76f5f
yeji <yeji.lee.kim@gmail.com>
2025-10-31 14:39:41 +0900

    [level 2] Title: 타겟 넘버, Time: 72.83 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=cpp) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 6.80 ms
+메모리: 3.67 MB, 시간: 72.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 28일 09:48:38
+2025년 10월 31일 14:39:40
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,22 +1,16 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n;
-int answer;
-
-void dfs(int cur, int sum, int target, vector<int>& v) {
-    if(cur == n) {
-        if(sum == target) answer++;
-    } else {
-        dfs(cur+1, sum + v[cur], target, v);
-        dfs(cur+1, sum - v[cur], target, v);
+int backtracking(vector<int> numbers, int target, int index, int sum) {
+    if(index == numbers.size()) {
+        return target == sum;
     }
+    
+    return backtracking(numbers, target, index + 1, sum + numbers[index])
+        + backtracking(numbers, target, index + 1, sum - numbers[index]);
 }
 
 int solution(vector<int> numbers, int target) {
-    n = numbers.size();
-    dfs(0, 0, target, numbers);
-    return answer;
+    return backtracking(numbers, target, 0, 0);
 }
